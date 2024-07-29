297a9b7a1fdf47857a0aad9109da5f2b9074469a
Jimmfly <yangjinfei001@gmail.com>
2024-07-29 23:06:10 +0800

    feat(core): add about affine component

--- packages/frontend/admin/src/modules/config/index.tsx
+++ packages/frontend/admin/src/modules/config/index.tsx
@@ -1,29 +1,22 @@
 import { Separator } from '@affine/admin/components/ui/separator';
-import { useQuery } from '@affine/core/hooks/use-query';
-import { serverConfigQuery } from '@affine/graphql';
 
 import { Layout } from '../layout';
+import { AboutAFFiNE } from './about';
 
 export function Config() {
   return <Layout content={<ConfigPage />} />;
 }
 
 export function ConfigPage() {
-  const {
-    data: { serverConfig },
-  } = useQuery({
-    query: serverConfigQuery,
-  });
-  console.log(serverConfig);
-  console.log(runtimeConfig);
-
   return (
     <div className=" h-screen flex-1 space-y-1 flex-col flex">
-      <div className="flex items-center justify-between px-6 py-3">
+      <div className="flex items-center justify-between px-6 py-3 max-md:ml-9">
         <div className="text-base font-medium">Config</div>
       </div>
       <Separator />
+      <AboutAFFiNE />
     </div>
   );
 }
+
 export { Config as Component };
